48164637c27ba07e263a90adb75e36c0efa7ca6d
QuyQuoc2002 <107377839+QuyQuoc2002@users.noreply.github.com>
2023-05-23 20:19:19 +0700

    commit

--- PRJSHOPASM/src/java/utils/Helper.java
+++ PRJSHOPASM/src/java/utils/Helper.java
@@ -23,5 +23,6 @@ public class Helper {
     public static void main(String[] args) {
         System.out.println(genRandSixDigit());
         System.out.println("123");
+        System.out.println("Quoc");
     }
 }
